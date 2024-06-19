0315e4a3f2cde63b243b31ca5d4402eb826a0c2e
Nicodav28 <esteveznicolas0@gmail.com>
2024-06-19 18:08:25 -0500

    ✨ (RecipeController.php): añadir funcionalidad para subir imágenes de recetas 📝 (StoreRecipeRequest.php): actualizar reglas de validación para imágenes
    Se ha añadido funcionalidad en el controlador de recetas para permitir la subida de imágenes de recetas. Ahora, al crear o actualizar una receta, se puede adjuntar una imagen que se almacenará en la carpeta 'recipes' dentro del directorio público. Además, se han actualizado las reglas de validación en la solicitud de almacenamiento de recetas para requerir que la imagen sea un archivo de tipo PNG. Esto mejora la experiencia del usuario al garantizar que solo se puedan subir imágenes válidas al sistema.

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -24,6 +24,9 @@ class RecipeController extends Controller
         $recipe = $request->user()->recipes()->create($request->all());
         $recipe->tags()->attach(json_decode($request->tags));
 
+        $recipe->image = $request->file('image')->store('recipes', 'public');
+        $recipe->save();
+
         return response()->json(new RecipeResource($recipe), Response::HTTP_CREATED);
     }
 
@@ -42,6 +45,11 @@ class RecipeController extends Controller
 
         if ($tags) {
             $recipe->tags()->sync($tags);
+        }
+
+        if ($request->file('image')) {
+            $recipe->image = $request->file('image')->store('recipes', 'public');
+            $recipe->save();
         }
 
         return response()->json(new RecipeResource($recipe), Response::HTTP_OK);
