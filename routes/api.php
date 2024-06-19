087a67ef8d36c1dee6ebe2c395a835fd6b9e057c
Nicodav28 <esteveznicolas0@gmail.com>
2024-06-19 17:47:36 -0500

    ✨ (LoginController.php): Crear controlador LoginController para manejar la autenticación de usuarios ✨ (User.php): Agregar HasApiTokens trait al modelo User para habilitar tokens de API 🔧 (composer.json): Actualizar la dependencia de laravel/tinker a cualquier versión 🔧 (api.php): Agregar ruta para el controlador LoginController y agrupar rutas protegidas
    Se creó un nuevo controlador LoginController para manejar la autenticación de usuarios, se agregó el trait HasApiTokens al modelo User para habilitar tokens de API, se actualizó la dependencia de laravel/tinker a cualquier versión y se agregó una nueva ruta para el controlador LoginController en el archivo api.php, además de agrupar las rutas protegidas.

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\RecipeController;
 use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\LoginController;
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('recipes', RecipeController::class);
-Route::apiResource('tags', TagController::class);
+Route::get('login', [LoginController::class, 'store']);
+
+Route::middleware('')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('recipes', RecipeController::class);
+    Route::apiResource('tags', TagController::class);
+});
